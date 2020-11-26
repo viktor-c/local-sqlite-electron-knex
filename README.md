e201e491a959f2dd8bcecf77f5d960b26fa15e10
Victor <victor.cighir@gmail.com>
2020-11-26 22:59:08 +0100

    initial upload of the files to the repository

--- README.md
+++ README.md
@@ -1,2 +1,41 @@
-# local-sqlite-electron-knex
-example using electron, local sqlite and knex.js
+![Logo](screenshots/knex.png)
+
+This project used the skeleton created by Patrick Moffit at https://github.com/patrickmoffitt/local-sqlite-example and was changed to use [knex.js](http://knexjs.org/)
+
+How to Build an Electron App with a Local SQLite3 Database That's Portable.
+------------------------------------------------------------------------
+
+This project demonstrates how to install and use a local SQLite3 database in Electron using [knex.js](http://knexjs.org/). 
+
+Getting started
+---------------
+ * install npm using this guide https://docs.npmjs.com/downloading-and-installing-node-js-and-npm
+ * create a directory on your computer and copy/clone this repository (`git clone https://github.com/viktor-c/local-sqlite-knex`) or download and extract .zip file
+ * open terminal, cd into the folder of this project
+ * Then run
+   - `npm install`
+   - `npm start`
+
+Quick Code Tour
+---------------
+
+ - The code entry point is in **package.json** under the key "main". The value is "main.js"
+ - **main.js** calls model.initDb() from **model.js** which creates a new SQLite3 database from **schema.sql**. A callback creates a BrowserWindow and loads **index.html**.
+ - **index.html** loads Bootstrap's CSS in the &lt;head&gt; tag and **renderer.js** after the closing &lt;body&gt; tag.
+ - **renderer.js** loads Cheerio, jQuery, Bootstrap and it's dependency; Tether.
+ - A [composed HTML pattern](https://github.com/patrickmoffitt/composed-html-example) is used to load the markup.
+ - The application's database work is all done in **model.js**. Here you'll find functions to select, insert, update, and delete records. 
+ - The application opens with a list of people. Click a red X to delete a person's record.
+
+ ![People](screenshots/people-list.png)
+ - Click a pencil (shown above) to edit a person:
+
+ ![Edit](screenshots/edit-person.png)
+ - Click Add in the left rail to add a new person record:
+
+ ![Add](screenshots/add-person.png)
+
+Uninstalling
+-------------
+
+ Please note that running this application will install a tiny SQLite database in the folder returned by electron.app.getPath('userData'). To uninstall simply delete this folder.
---
+++ app/model.js
@@ -0,0 +1,104 @@
+'use strict'
+
+const path = require('path')
+const fs = require('fs')
+const SQL = require('sql.js')
+const view = require(path.join(__dirname, 'view.js'))
+
+/*
+Knex init DB
+must run 
+npm install sqlite3 --save
+*/
+const knex = require("knex")({
+	client: "sqlite3",
+	connection: {
+		filename: path.join(__dirname, 'example.db')
+	}
+});
+
+
+/**
+ * Knex getPeople
+ * Populates the People List. Send it to the view
+ * @param {} object 
+ */
+module.exports.getPeople = function () {
+  let result = knex.select().from("people")
+  result.then(function(rows){
+    view.showPeople(rows)
+  })
+}
+
+
+/**
+ * Knex getPerson
+ *   Fetch a person's data from the database.
+ * @param {} object 
+ */
+/*
+  Populates the People List. Send it to the view
+*/
+module.exports.getPerson = async function (pid) {
+  console.log("modulejs.getPerson " + pid)
+  let result = await knex.select().from("people").where("person_id", pid)
+  return result
+  /*result.then(
+    //fullfiled
+    function(rows){
+      console.log("module.getPerson result rows")
+      console.log(rows[0])
+      return rows[0]
+    },
+    //rejected
+    error => {
+      console.log('model.getPerson', error.message)
+    })*/
+}
+
+/*
+  Delete a person's data from the database.
+*/
+module.exports.deletePerson = function (pid, callback) {
+  
+  let result = knex.from("people").where("person_id", pid).delete()
+  result.then(
+    //fullfiled
+    ()=>{
+      if (typeof callback === 'function') {
+        callback()
+      }
+    },
+    //rejected
+    error => {
+      console.log('model.deletePerson', error.message)
+    })
+}
+
+
+/*
+  Insert or update a person's data in the database.
+*/
+module.exports.saveFormData = function (tableName, keyValue, callback) {
+  if (keyValue.columns.length > 0) {
+      let newRow = {}
+      for (let i=0; i<keyValue.columns.length;i++) {
+          newRow[keyValue.columns[i]] = keyValue.values[i]
+      }
+      let result = knex(tableName).insert(newRow).onConflict("person_id").merge()
+      result.then(
+        function(){
+          $('#' + keyValue.columns.join(', #'))
+            .addClass('form-control-success')
+            .animate({class: 'form-control-success'}, 1500, function () {
+              if (typeof callback === 'function') {
+                callback()
+              }
+            })
+        },
+        //not fullfiled
+        function(){console.log('model.saveFormData', 'Query failed for', keyValue.values)}
+      )
+      
+  }
+}
